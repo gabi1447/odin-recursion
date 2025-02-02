e3ebf4f14d4736a00d13bdad92a0f7026b99b552
Gabriel <gabrielpachecotec@gmail.com>
2025-02-02 13:09:57 +0000

    Write function that returns fibonacci sequence when a number is given using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -20,3 +20,28 @@ function fibs(sequenceLength) {
 
     return sequenceArray;
 }
+
+// Recursive approach
+function fibsRec(sequenceLength) {
+    // base case
+    if (sequenceLength === 0) {
+        return [];
+    } else if (sequenceLength === 1) {
+        return [0];
+    } else if (sequenceLength === 2) {
+        return [0, 1];
+    }
+
+    // recursive call
+    const previousSequence = fibsRec(sequenceLength - 1);
+    const length = previousSequence.length;
+    return previousSequence.concat(
+        previousSequence[length - 2] + previousSequence[length - 1]
+    );
+}
+
+const result = fibs(10);
+console.log(result);
+
+const resultRec = fibsRec(10);
+console.log(resultRec);
