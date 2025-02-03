cca5b541a024750817b17a9319800584275680e1
Gabriel <gabrielpachecotec@gmail.com>
2025-02-03 12:14:54 +0000

    Write spitArray function

--- mergeSort.js
+++ mergeSort.js
@@ -26,3 +26,12 @@ function merge(array1, array2) {
 
     return sortedArray;
 }
+
+function splitArray(array) {
+    const arrayLength = array.length;
+    middle = arrayLength / 2;
+    middle = middle % 2 === 0 ? middle : Math.round(middle);
+    const leftArray = array.slice(0, middle);
+    const rightArray = array.slice(middle);
+    return [leftArray, rightArray];
+}
